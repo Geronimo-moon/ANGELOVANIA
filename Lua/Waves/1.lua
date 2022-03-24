88ed84fd2e9a140e7da81052e4f4fc84041f7a5d
Silvia-moon <Euler.ecosisin@gmail.com>
2022-03-24 18:37:23 +0900

    Update 1.lua

--- Lua/Waves/1.lua
+++ Lua/Waves/1.lua
@@ -7,20 +7,22 @@ knives = {}
 function Update()
   frame = frame + 1
 
-  if frame % 24 == 3 then
+  if frame % 16 == 3 then
     local spawn = math.random(-Arena.height/2,Arena.height/2)
     for i=1,10 do
       local knife = SetNotime("knifer",Arena.width,spawn)
+      knife.ppcollision = true
       knife.SetVar("angle",(i-1)*36)
       knife.sprite.rotation = (i-1)*36
       table.insert(knives,knife)
     end
   end
 
-  if frame % 24 == 15 then
+  if frame % 16 == 11 then
     local spawn = math.random(-Arena.height/2,Arena.height/2)
     for i=1,10 do
       local knife = SetNotime("knifer",-Arena.width,spawn)
+      knife.ppcollision = true
       knife.SetVar("angle",(i-1)*36)
       knife.sprite.rotation = (i-1)*36
       table.insert(knives,knife)
@@ -30,8 +32,8 @@ function Update()
   for i=1,#knives do
     if knives[i].isactive then
       local theta = math.pi*knives[i].GetVar("angle")/180
-      local x = 5*math.cos(theta)
-      local y = 5*math.sin(theta)
+      local x = 7*math.cos(theta)
+      local y = 7*math.sin(theta)
       knives[i].Move(x,y)
 
       if knives[i].absx <= 0 or knives[i].absx >= 640 or knives[i].absy <= 0 or knives[i].absy >= 480 then
