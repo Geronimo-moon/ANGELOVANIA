0fac0477856bfc4ea7513880bc6b7db95dddd05f
Geronimo-moon <Euler.ecosisin@gmail.com>
2022-04-24 20:32:31 +0900

    add blaster library

--- Lua/Libraries/blaster.lua
+++ Lua/Libraries/blaster.lua
@@ -11,17 +11,23 @@ function SetBlaster(name,x,y,layer,color)
     color = "white"
   end
 
-  local blaster = SetBeam(path,x,y,layer,color)
+  local blaster = SetSprite(path,x,y,layer)
 
   blaster.SetVar('name',name)
 
   blaster.SetVar('name',name)
+
+  Audio.PlaySound('blaster')
 
   return blaster
 end
 
-function UpdateBlaster(blaster,frame)
+function UpdateBlaster(blaster,frame,time)
   local name = blaster.GetVar('name')
+
+  if time == nil or time <= 100 then
+    time = 100
+  end
 
   if name == 'gaster' then
     if blaster.GetVar('frame') == nil then
@@ -35,19 +41,35 @@ function UpdateBlaster(blaster,frame)
     elseif spawn == 47 then
       blaster.sprite.Set('attack/'..name..'3')
     elseif spawn >= 50 then
-      if spawn%6 == 2 then
+      if spawn%8 == 2 then
         blaster.sprite.Set('attack/'..name..'4')
-      elseif spawn%6 == 5 then
+      elseif spawn%8 == 6 then
         blaster.sprite.Set('attack/'..name..'5')
       end
 
       if spawn == 50 then
-        local beam = SetBeam('slash/slashb',blaster.x,blaster.y)
-        beam.ppcollision = true
+        local beam = SetBeam('slash/slashb',blaster.x+800*math.cos(math.pi*blaster.sprite.rotation/180),blaster.y+800*math.sin(math.pi*blaster.sprite.rotation/180))
+        beam.sprite.Scale(1,5/2*blaster.sprite.yscale)
+        beam.sprite.color = hsvToRgb(0,255,200)
         beam.sprite.rotation = blaster.sprite.rotation
         blaster.SetVar('beam',beam)
       end
+
+      blaster.GetVar('beam').sprite.yscale = (2+math.cos((frame-50)*math.pi/4)/2)*blaster.sprite.yscale
+
+      blaster.Move(-10*math.cos(math.pi*blaster.sprite.rotation/180),-10*math.sin(math.pi*blaster.sprite.rotation/180))
+      blaster.GetVar('beam').Move(-10*math.cos(math.pi*blaster.sprite.rotation/180),-10*math.sin(math.pi*blaster.sprite.rotation/180))
+
+      if time - spawn <= 10 then
+        blaster.sprite.alpha = blaster.sprite.alpha - 0.1
+        blaster.GetVar('beam').sprite.alpha = blaster.GetVar('beam').sprite.alpha - 0.1
+        if blaster.sprite.alpha == 0 then
+          blaster.Remove()
+          blaster.GetVar('beam').Remove()
+        end
+      end
     end
+
   elseif name == 'chaos' then
     if blaster.GetVar('frame') == nil then
       blaster.SetVar('frame',frame)
@@ -58,9 +80,36 @@ function UpdateBlaster(blaster,frame)
     if spawn == 50 then
       blaster.sprite.Set('attack/'..name..'2')
       local beam = SetBeam('slash/slashb',blaster.x,blaster.y)
-      beam.ppcollision = true
+      beam.sprite.Scale(1,blaster.sprite.yscale)
       beam.sprite.rotation = blaster.sprite.rotation
+      beam.sprite.color = hsvToRgb((spawn-50)%255,255,200)
+      beam.sprite.color[1] = 1
+      beam.sprite.alpha = 0.5
+      local center = SetSprite('slash/slashb',blaster.x,blaster.y)
+      center.sprite.Scale(1,blaster.sprite.yscale/2)
+      center.sprite.rotation = blaster.sprite.rotation
+      center.sprite.color = hsvToRgb(0,175,255)
       blaster.SetVar('beam',beam)
+      blaster.SetVar('center',center)
+    end
+
+    if spawn >= 50 then
+      blaster.Move(-10*math.cos(math.pi*blaster.sprite.rotation/180),-10*math.sin(math.pi*blaster.sprite.rotation/180))
+      blaster.GetVar('center').Move(-10*math.cos(math.pi*blaster.sprite.rotation/180),-10*math.sin(math.pi*blaster.sprite.rotation/180))
+      blaster.GetVar('beam').Move(-10*math.cos(math.pi*blaster.sprite.rotation/180),-10*math.sin(math.pi*blaster.sprite.rotation/180))
+      blaster.GetVar('beam').sprite.color = hsvToRgb((spawn-50)*4%255,255,200)
+      blaster.GetVar('beam').sprite.color[1] = 1
+
+      if time - spawn <= 5 then
+        blaster.sprite.alpha = blaster.sprite.alpha - 0.2
+        blaster.GetVar('beam').sprite.alpha = blaster.GetVar('beam').sprite.alpha - 0.1
+        blaster.GetVar('center').sprite.alpha = blaster.GetVar('center').sprite.alpha - 0.1
+        if blaster.sprite.alpha == 0 then
+          blaster.Remove()
+          blaster.GetVar('beam').Remove()
+          blaster.GetVar('center').Remove()
+        end
+      end
     end
   end
 end
--- Lua/Waves/15.lua
+++ Lua/Waves/15.lua
@@ -3,21 +3,25 @@ require 'Libraries.blaster'
 
 frame = 0
 
+Arena.Resize(400,360)
+
 knives = {}
 blasters = {}
+
+SetPPCollision(true)
 
 function Update()
   frame = frame + 1
 
   if frame == 1 then
-    local blst = SetBlaster('gaster',-150,150)
+    local blst = SetBlaster('chaos',-10,10)
     blst.sprite.rotation = -45
     table.insert(blasters,blst)
   end
 
   for i = 1, #blasters do
     if blasters[i].isactive then
-      UpdateBlaster(blasters[i],frame)
+      UpdateBlaster(blasters[i],frame,600)
     end
   end
 end
